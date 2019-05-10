4afb83fd374efb492efa7b449fa6fc248f6b249c
sebastiangreger <sebastiangreger@users.noreply.github.com>
2019-05-10 14:28:29 +0200

    Add auto-refresh to panel component

--- index.js
+++ index.js
@@ -13,13 +13,26 @@ panel.plugin('sgkirby/sendmentions', {
 					this.headline = response.headline;
 					this.pings    = response.pings;
 				});
+				this.$events.$on("model.update", this.refresh);
+			},
+			destroyed: function() {
+				this.$events.$off("model.update", this.refresh);
 			},
 			template: `
 				<section class="k-pings-section">
-					<k-headline>{{ headline }}</k-headline>
+					<header class="k-section-header">
+						<k-headline>{{ headline }}</k-headline>
+					</header>
 					<k-text><span v-html="pings"></span></k-text>
 				</section>
-			`
+			`,
+			methods: {
+				refresh(){
+					this.load().then(response => {
+						this.pings    = response.pings;
+					});
+				}
+			}
 		}
 	}	
 
