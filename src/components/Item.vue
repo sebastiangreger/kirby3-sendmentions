1b7edbfbaa28c137dcd7f7efae55001bbe202c13
sebastiangreger <sebastiangreger@users.noreply.github.com>
2020-05-06 12:31:28 +0200

    Update UI and styles

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,17 +1,20 @@
 <template>
-  <li class="k-list-item">
+  <li class="k-list-item" :class="itemclass">
     <div class="k-list-item-content">
-      <span class="k-list-item-image">
-        <k-icon :type="icon"/>
-      </span>
-      <span class="k-list-item-text">
-        <em
-          v-html="item.target"
-        />
-        <small
-          v-html="type_formatted"
-        />
-      </span>
+
+      <a :href="itemlink" target="_blank" class="k-link k-list-item-content">
+        <span class="k-list-item-image">
+          <k-icon :type="icon"/>
+        </span>
+        <span class="k-list-item-text">
+          <em
+            v-html="item.target"
+          />
+          <small
+            v-html="type_formatted"
+          />
+        </span>
+      </a>
     </div>
     <nav class="k-list-item-options">
       <k-button
@@ -38,12 +41,26 @@ export default {
   },
 
   computed: {
+    itemlink() {
+      if (this.item.type == 'archive.org')
+        return this.item.data.url;
+      else
+        return this.item.target;
+    },
+    itemclass() {
+      if (this.item.type == 'archive.org' || this.item.type == 'pingback' || this.item.type == 'webmention')
+        return 'k-list-item-sendmentions-sent';
+      else
+        return 'k-list-item-sendmentions-failed';
+    },
     type_formatted() {
       switch (this.item.type) {
         case 'none':
-            return '';
+          return '';
+        case 'webmention':
+          return this.item.type + ': ' + this.item.data.response;
         default:
-            return this.item.type;
+          return this.item.type;
       }
     },
     icon() {
@@ -53,7 +70,7 @@ export default {
         case 'pingback':
             return 'globe';
         case 'archive.org':
-            return 'download';
+            return 'sendmentions-archiveorg';
         case 'none':
             return 'cancel';
         default:
@@ -75,5 +92,7 @@ export default {
 </script>
 
 <style lang="scss">
-
+.k-list-item-sendmentions-sent .k-list-item-image .k-icon svg * { fill:green; }
+.k-list-item-sendmentions-failed .k-list-item-image .k-icon svg * { fill:red; }
+.k-sendmentions-section .k-list-item-text small { text-transform: capitalize; }
 </style>
